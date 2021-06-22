20683b2801b4216de16f5439a6d437303efc5632
poseng <poseng.than@gmail.com>
2021-06-22 13:42:37 +0200

    add TestNew.cpp

---
+++ src/TestNew.cpp
@@ -0,0 +1,11 @@
+#include "MovablePoint.h"
+#include "Point.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello Point " << endl;
+	cout << "Hello MovablePoint: " << endl;
+	return 0;
+}
